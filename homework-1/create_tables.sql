edac572e3d693e981999fdc54e32e1bde18cab26
kasyan1337 <tristokorun@gmail.com>
2024-03-17 22:41:04 +0800

    Tables done, main added logic without transferring from the data file

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,33 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE customers
+(
+	customer_id varchar(5) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+SELECT * FROM customers;
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+SELECT * FROM employees;
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(5) NOT NULL,
+	employee_id int NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
+);
+
+SELECT * FROM orders;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,40 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='1337', port='5433')
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+
+            # ##### CUSTOMERS TABLE #####
+
+            cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", (customer_id, company_name, contact_name))
+
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+
+            # ##### EMPLOYEES TABLE #####
+
+            cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+                        (employee_id, first_name, last_name, title, birth_date, notes))
+
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+
+            # ##### ORDERS TABLE #####
+
+
+            cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+                        (order_id, customer_id_o, employee_id_o, order_date, ship_city))
+
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+
+            # conn.commit() # not needed, because conn is opened with 'with', so it automatically commits
+
+
+finally:
+    conn.close()  # must be closed here
