1ea6fd3724e92c62827f223d3881fb66d34daadc
kasyan1337 <tristokorun@gmail.com>
2024-03-17 22:57:32 +0800

    Homework 1 - Done

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,3 +1,5 @@
+import csv
+
 import psycopg2
 
 conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='1337', port='5433')
@@ -5,33 +7,31 @@ conn = psycopg2.connect(host='localhost', database='north', user='postgres', pas
 try:
     with conn:
         with conn.cursor() as cur:
-
             # ##### CUSTOMERS TABLE #####
 
-            cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", (customer_id, company_name, contact_name))
-
-            rows = cur.fetchall()
-            for row in rows:
-                print(row)
+            with open('north_data/customers_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)  # Skip the header row
+                for customer_id, company_name, contact_name in reader:
+                    cur.execute("INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",
+                                (customer_id, company_name, contact_name))
 
             # ##### EMPLOYEES TABLE #####
-
-            cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+            with open('north_data/employees_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)  # Skip the header row
+                for employee_id, first_name, last_name, title, birth_date, notes in reader:
+                    cur.execute(
+                        "INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s, %s)",
                         (employee_id, first_name, last_name, title, birth_date, notes))
 
-            rows = cur.fetchall()
-            for row in rows:
-                print(row)
-
             # ##### ORDERS TABLE #####
-
-
-            cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
-                        (order_id, customer_id_o, employee_id_o, order_date, ship_city))
-
-            rows = cur.fetchall()
-            for row in rows:
-                print(row)
+            with open('north_data/orders_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)  # Skip the header row
+                for order_id, customer_id, employee_id, order_date, ship_city in reader:
+                    cur.execute("INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s, %s)",
+                                (order_id, customer_id, employee_id, order_date, ship_city))
 
             # conn.commit() # not needed, because conn is opened with 'with', so it automatically commits
 
