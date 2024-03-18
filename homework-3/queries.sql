07714dd105e1b9ff9f9b0bc9e78f3be0e2f2c468
kasyan1337 <tristokorun@gmail.com>
2024-03-19 01:21:54 +0800

    Homework 3 - Done

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,15 +2,63 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
+SELECT
+    c.company_name,
+    CONCAT(e.first_name, ' ', e.last_name) AS fio
+--    e.first_name || ' ' || e.last_name AS fio # same as CONCAT
+FROM
+    orders o
+JOIN
+    customers c ON o.customer_id = c.customer_id
+JOIN
+    employees e ON o.employee_id = e.employee_id
+JOIN
+    shippers s ON o.ship_via = s.shipper_id
+WHERE
+    c.city = 'London'
+    AND e.city = 'London'
+    AND s.company_name = 'United Package';
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
+SELECT
+    p.product_name,
+    p.units_in_stock,
+    s.contact_name,
+    s.phone
+FROM
+    products p
+JOIN
+    suppliers s ON p.supplier_id = s.supplier_id
+JOIN
+    categories c ON p.category_id = c.category_id
+WHERE
+    p.discontinued = 0
+    AND p.units_in_stock < 25
+    AND c.category_name IN ('Dairy Products', 'Condiments')
+ORDER BY
+    p.units_in_stock ASC;
 
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
+SELECT
+    c.company_name
+FROM
+    customers c
+LEFT JOIN
+    orders o ON c.customer_id = o.customer_id
+WHERE
+    o.order_id IS NULL;
 
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT DISTINCT p.product_name
+FROM products p
+JOIN (
+    SELECT distinct product_id
+    FROM order_details
+    WHERE quantity = 10
+) as od ON p.product_id = od.product_id;
